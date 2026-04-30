bf1ebb13d3ab4c5f9316d7bd19a0335e0fd983de
huaji <huaji>
2026-04-30 18:42:06 +0800

    chore: rename evt brand and clean unused assets

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, esmExternalRequirePlugin } from 'vite';
+import { defineConfig } from 'vite';
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
